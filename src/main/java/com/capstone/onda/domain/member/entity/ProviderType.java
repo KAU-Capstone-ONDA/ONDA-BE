b5a558785a772ed14779033d1e6aaffa35ec0700
Yurim Yang <yang980329@likelion.org>
2024-06-07 21:03:13 +0900

    feat : Member Entity 수정 및 ProviderType 수정
    - providerType 추가
    - Hotel 과 oneToOne 관계 생성

--- src/main/java/com/capstone/onda/domain/member/entity/ProviderType.java
+++ src/main/java/com/capstone/onda/domain/member/entity/ProviderType.java
@@ -1,9 +1,27 @@
 package com.capstone.onda.domain.member.entity;
 
+import com.capstone.onda.domain.member.exception.InvalidProviderTypeException;
+import com.capstone.onda.global.exception.ErrorCode;
 import lombok.Getter;
 
 @Getter
 public enum ProviderType {
-    KAKAO, LOCAL;
+    KAKAO("KAKAO"),
+    NAVER("NAVER");
+
+    private final String value;
+
+    ProviderType(String value){
+        this.value = value;
+    }
+
+    public static ProviderType toProviderType(String loginProviderType) {
+        for (ProviderType providerType : values()) {
+            if (providerType.value.equalsIgnoreCase(loginProviderType)) {
+                return providerType;
+            }
+        }
+        throw new InvalidProviderTypeException(ErrorCode.INVALID_PROVIDER_TYPE_EXCEPTION);
+    }
 }
 
