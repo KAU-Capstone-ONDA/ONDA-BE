69cf7c510c3bd5b6ee24c66244f4a321b1b278ae
heg-git <ghddmsrl1@naver.com>
2024-06-09 16:06:21 +0900

    feat: 호텔 Entity의 '등급' 속성 추가 - 호텔 Entity 사용 코드 수정

--- src/main/java/com/capstone/onda/domain/member/dto/response/MemberResponse.java
+++ src/main/java/com/capstone/onda/domain/member/dto/response/MemberResponse.java
@@ -8,17 +8,19 @@ public record MemberResponse(
     String name,
     String hotelName,
     String region,
-    String city
+    String city,
+    Integer star
 ) {
 
     @Builder
     public MemberResponse(Long id, String email, String name, String hotelName, String region,
-        String city) {
+        String city, Integer star) {
         this.id = id;
         this.email = email;
         this.name = name;
         this.hotelName = hotelName;
         this.region = region;
         this.city = city;
+        this.star = star;
     }
 }
