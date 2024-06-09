b5460f2468e67e6ee0f787ec0b27d60a96402998
heg-git <ghddmsrl1@naver.com>
2024-06-09 16:12:07 +0900

    feat: 특정 호텔 조회 API 구현 - HotelResponse Class 추가 - HotelMapper Class에 Mapper Method 추가 - Hotel RestController, Service, Repository 구현

---
+++ src/main/java/com/capstone/onda/domain/hotel/controller/HotelRestController.java
@@ -0,0 +1,30 @@
+package com.capstone.onda.domain.hotel.controller;
+
+import com.capstone.onda.domain.hotel.dto.response.HotelResponse;
+import com.capstone.onda.domain.hotel.service.HotelService;
+import com.capstone.onda.global.common.ResponseDTO;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("v1")
+@RequiredArgsConstructor
+public class HotelRestController {
+
+    private final HotelService hotelService;
+
+    @GetMapping("/hotel")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseDTO<List<HotelResponse>> getAllHotels(@RequestParam String name) {
+        return ResponseDTO.res(hotelService.getHotel(name), "hotel return success");
+    }
+
+}
---
+++ src/main/java/com/capstone/onda/domain/hotel/service/HotelService.java
@@ -0,0 +1,39 @@
+package com.capstone.onda.domain.hotel.service;
+
+import com.capstone.onda.domain.hotel.dto.response.HotelResponse;
+import com.capstone.onda.domain.hotel.entity.Hotel;
+import com.capstone.onda.domain.hotel.exception.HotelNotFound;
+import com.capstone.onda.domain.hotel.repository.HotelRepository;
+import com.capstone.onda.domain.hotel.util.HotelMapper;
+import com.capstone.onda.global.exception.ErrorCode;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class HotelService {
+
+    private final HotelRepository hotelRepository;
+
+    public List<HotelResponse> getHotel(String hotelName) {
+        if (hotelName.isBlank()) {
+            throw new HotelNotFound(ErrorCode.INVALID_HOTEL_EXCEPTION);
+        }
+
+        String replacedHotel = hotelName.replace(" ", "");
+        List<Hotel> hotels = hotelRepository.findAllByHotelName(replacedHotel);
+
+        if (hotels.isEmpty()) {
+            throw new HotelNotFound(ErrorCode.INVALID_HOTEL_EXCEPTION);
+        }
+
+        return hotels.stream()
+            .map(HotelMapper::toHotelResponse)
+            .collect(Collectors.toList());
+    }
+
+}
