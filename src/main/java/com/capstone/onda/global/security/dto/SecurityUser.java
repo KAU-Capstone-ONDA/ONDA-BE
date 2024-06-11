e275183a7bfee97fedf27a3a885d3f56f86f062e
Yurim Yang <yang980329@likelion.org>
2024-06-11 21:39:26 +0900

    fix: SecurityUser 속성 추가
    - role, providerType, isExist 속성 추가
    - dto로 사용 변경

--- src/main/java/com/capstone/onda/global/security/dto/SecurityUser.java
+++ src/main/java/com/capstone/onda/global/security/dto/SecurityUser.java
@@ -5,7 +5,9 @@ import lombok.Builder;
 public record SecurityUser(
     String email,
     String nickname,
-    String role
+    String role,
+    String providerType,
+    boolean isExist
 ) {
 
     @Builder
